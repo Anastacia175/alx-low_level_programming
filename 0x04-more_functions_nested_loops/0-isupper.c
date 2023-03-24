673a0bf078e94c977fb4c96c8f38152d2cc57d18
Anastacia175 <annsoniag52@gmail.com>
2023-03-24 01:07:52 -0700

    Isuppert

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
   *_isupper - shows if the input is uppercase
   *character and returns 1 else returns 0
@@ -9,10 +10,9 @@
   */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 		return (0);
-
 }
