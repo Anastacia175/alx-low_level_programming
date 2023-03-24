2dfa5bf9f7185d76110813ea3aef19b8dc5c111c
Anastacia175 <annsoniag52@gmail.com>
2023-03-24 01:41:49 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isupper(c) - shows if the input is uppercase
+  *character and returns 1 else returns 0
+  *@c: The character to be checked
+  *Return: 1 or 0
+  */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+		return (0);
+
+}
