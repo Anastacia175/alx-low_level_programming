ea91d4fb02f83941bcf1eb3408899d081c4c2652
Anastacia175 <annsoniag52@gmail.com>
2023-04-12 14:07:32 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /**
-* check_num - check - string there are digit
-* @str: array str
-*
-* Return: Always 0 (Success)
-*/
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+
 int check_num(char *str)
+
 {
 /*Declaring variables*/
 unsigned int count;
-
 
 count = 0;
 while (count < strlen(str)) /*count string*/
@@ -28,26 +30,21 @@ count++;
 return (1);
 }
 
-
 /**
-* main - Print the name of the program
-* @argc: Count arguments
-* @argv: Arguments
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 
-
 {
-
-
 /*Declaring variables*/
 int count;
+int str_to_int;
 int sum = 0;
-
 
 count = 1;
 while (count < argc) /*Goes through the whole array*/
@@ -59,7 +56,6 @@ str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
 
-
 /*Condition if one of the number contains symbols that are not digits*/
 else
 {
@@ -70,8 +66,8 @@ return (1);
 count++;
 }
 
-
 printf("%d\n", sum); /*print sum*/
 
 return (0);
 }
+
