85df28b9997f62acdb36c7b54db637b9f5ef7fc9
Anastacia175 <annsoniag52@gmail.com>
2023-04-12 13:37:22 -0700

    make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,88 +1,104 @@
 #include <stdio.h>
-		
 #include <stdlib.h>
-		
 #include "main.h"
-		
 
-		
 /**
-		
  * main - prints the minimum number of coins to
-		
  * make change for an amount of money
-		
  * @argc: number of arguments
-		
  * @argv: array of arguments
-		
  *
-		
  * Return: 0 (Success), 1 (Error)
-		
  */
-		
+
 int main(int argc, char *argv[])
-		
+
 {
-		
-	int num, j, result;
-		
-	int coins[] = {25, 10, 5, 2, 1};
-		
 
-		
-	if (argc != 2)
-		
-	{
-		
-		printf("Error\n");
-		
-		return (1);
-		
-	}
-		
+int num, j, result;
 
-		
-	num = atoi(argv[1]);
-		
-	result = 0;
-		
+int coins[] = {25, 10, 5, 2, 1};
 
-		
-	if (num < 0)
-		
-	{
-		
-		printf("0\n");
-		
-		return (0);
-		
-	}
-		
-
-		
-	for (j = 0; j < 5 && num >= 0; j++)
-		
-	{
-		
-		while (num >= coins[j])
-		
-		{
-		
-			result++;
-		
-			num -= coins[j];
-		
-		}
-		
-	}
-		
-
-		
-	printf("%d\n", result);
-		
-	return (0);
-		
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
 
+num = atoi(argv[1]);
+
+result = 0;
+
+if (num < 0)
+
+{
+printf("0\n");
+return (0);
+
+}
+
+for (j = 0; j < 5 && num >= 0; j++)
+
+{
+
+while (num >= coins[j])
+
+{
+result++;
+num -= coins[j];
+}
+}
+printf("%d\n", result);
+
+return (0);
+
+}
+
+
+
+/**
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(int argc, char *argv[])
+
+
+{
+
+
+/*Declaring variables*/
+int count;
+int sum = 0;
+
+
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if (check_num(argv[count]))
+
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+
+
+/*Condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
+
+count++;
+}
+
+
+printf("%d\n", sum); /*print sum*/
+
+return (0);
+}
